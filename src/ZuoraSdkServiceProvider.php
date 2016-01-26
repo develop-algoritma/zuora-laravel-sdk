dcd835e3d2a9de640b454a1db8dbdd6653cbe16e
Igor Osharin <igor@osharin.ru>
2016-01-26 23:39:55 +0300

    ServiceProvider

--- src/ZuoraSdkServiceProvider.php
+++ src/ZuoraSdkServiceProvider.php
@@ -1,9 +1,12 @@
-<?php namespace Spira\ZuoraSdk;
+<?php
 
+namespace Spira\ZuoraSdk;
+
+use Psr\Log\LoggerInterface;
 use Illuminate\Support\ServiceProvider;
 
 /**
- * Class DatabaseServiceProvider
+ * Class DatabaseServiceProvider.
  */
 class ZuoraSdkServiceProvider extends ServiceProvider
 {
@@ -14,7 +17,16 @@ class ZuoraSdkServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
+        $this->registerApi($this->app['config']['zuora'], $this->app[LoggerInterface::class]);
     }
 
+    protected function registerApi($config, LoggerInterface $logger = null)
+    {
+        $this->app->bind(
+            API::class,
+            function () use ($config, $logger) {
+                return new API($config, $logger);
+            }
+        );
+    }
 }
